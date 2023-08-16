59f96bc5fb0e0892ad574279a0cf914ec60bc034
matt jaehn <infoprol@icloud.com>
2023-08-16 03:39:31 -0500

    got the suggestions dropdown positioned properly.

--- src/components/products/SearchBar.tsx
+++ src/components/products/SearchBar.tsx
@@ -22,20 +22,27 @@ interface Suggestion {
 
 const SuggestionsDropdown = ({
   suggestions,
+  showSuggestions,
 }: {
   suggestions: Suggestion[];
+  showSuggestions: boolean;
 }) => {
   //
   // <label for="countries_multiple" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+
+  if (!showSuggestions) return <></>;
   return (
     <>
       <select
         multiple
         id="suggestions"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className={
+          `${showSuggestions ? "" : "hidden "}` +
+          "absolute bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        }
       >
         {suggestions.map(({ id, text }) => (
-          <option key={id} value={id}>
+          <option key={id} value={id} className="block px-4 rounded-sm">
             {text}
           </option>
         ))}
@@ -57,6 +64,8 @@ function SearchBar({
   const [suggestionsOpen, setSuggestionsOpen] = useState<boolean>(true);
   const [currSelectedSuggestion, setCurrSelectedSuggestion] =
     useState<number>(-1);
+
+  const showSuggestions = suggestionsOpen && searchText !== "";
 
   const searchBarRef = useRef(null);
   useOutsideClick(searchBarRef, () => setSuggestionsOpen(false));
@@ -107,7 +116,7 @@ function SearchBar({
     };
   });
 
-  const suggestions = !suggestionsOpen || searchText === "" ? [] : nameSuggests; //.concat(tagSuggests);
+  const suggestions = !showSuggestions ? [] : nameSuggests; //.concat(tagSuggests);
 
   const handleSearchInputKeys: KeyboardEventHandler<HTMLInputElement> = async (
     evt,
@@ -158,37 +167,37 @@ function SearchBar({
   //
   //   return (
   //     <div>
-  //       <div className="relative overflow-hidden">
+  //       <div className=" overflow-hidden">
   //         <div
-  //           className="bg-[#fffebe] position-relative justify-center ml-1/2 w-1/3 z-100"
+  //           className="bg-[#fffebe] position- justify-center ml-1/2 w-1/3 z-100"
   //           ref={searchBarRef}
   //         >
   //           <input
   //             type="text"
   //             placeholder="search..."
-  //             className="absolute text-center color-black decoration-none min-h-[3rem] rounded-md w-full flex flex-row justify-center text-sm p-[0.2rem] z-6 bg-pink-400 border-l-4 border-color-green ml-1/2"
+  //             className=" text-center color-black decoration-none min-h-[3rem] rounded-md w-full flex flex-row justify-center text-sm p-[0.2rem] z-6 bg-pink-400 border-l-4 border-color-green ml-1/2"
   //             // onChange={handleSearch}
   //             onChange={(e) => setSearchText(e.target.value)}
   //             value={searchText}
   //             //onKeyDown={handleSearchInputKeys}
   //           />
-  //           <div className="text-sm flex flex-col justify-center w-full border-t-0 divide-y divide-indigo-50 mt-0 overflow-auto z-5 absolute rounded-md bg-color-burlywood opacity-90">
+  //           <div className="text-sm flex flex-col justify-center w-full border-t-0 divide-y divide-indigo-50 mt-0 overflow-auto z-5  rounded-md bg-color-burlywood opacity-90">
   //             <SuggestionsDropdown suggestions={suggestions} />
   //             {/* {suggestions.map((suggestion, indx) => (
-  //               <div
-  //                 className="text-sm font-normal p-[0.2rem] text-center color-black decoration-none min-h-[24px] justify-center"
-  //                 key={indx}
-  //                 // onKeyDown={suggestion.onKeyDown}
-  //                 onClick={suggestion.onClick}
-  //               >
-  //                 <span
+  //                 <div
+  //                   className="text-sm font-normal p-[0.2rem] text-center color-black decoration-none min-h-[24px] justify-center"
+  //                   key={indx}
   //                   // onKeyDown={suggestion.onKeyDown}
   //                   onClick={suggestion.onClick}
   //                 >
-  //                   {suggestion.text}
-  //                 </span>
-  //               </div>
-  //             ))} */}
+  //                   <span
+  //                     // onKeyDown={suggestion.onKeyDown}
+  //                     onClick={suggestion.onClick}
+  //                   >
+  //                     {suggestion.text}
+  //                   </span>
+  //                 </div>
+  //               ))} */}
   //           </div>
   //         </div>
   //       </div>
@@ -196,80 +205,45 @@ function SearchBar({
   //   );
 
   return (
-    <div className="flex">
-      <div className="relative w-full">
-        <input
-          type="search"
-          id="search-dropdown"
-          className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-          placeholder="Search Mockups, Logos, Design Templates..."
-          required
-          onChange={(e) => setSearchText(e.target.value)}
-          value={searchText}
-        />
-        <button
-          type="submit"
-          className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          <svg
-            className="w-4 h-4"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
+    <div className="flex justify-center items-center">
+      <div className=" w-1/3 relative">
+        <div>
+          <input
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+            placeholder="Search..."
+            required
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
+          />
+          {/* <button
+            type="submit"
+            className=" absolute stop-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-          <span className="sr-only">Search</span>
-        </button>
-      </div>
-      <div
-        id="dropdown"
-        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-      >
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdown-button"
-        >
-          <li>
-            <button
-              type="button"
-              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
             >
-              Mockups
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Templates
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Design
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Logos
-            </button>
-          </li>
-        </ul>
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search</span>
+          </button> */}
+        </div>
+
+        <SuggestionsDropdown
+          suggestions={suggestions}
+          showSuggestions={showSuggestions}
+        />
       </div>
     </div>
   );
